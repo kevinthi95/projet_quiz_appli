134775a4561cceff41c2432374b9394964044767
CYShop <133589385+CYShop@users.noreply.github.com>
2024-05-03 14:15:11 +0200

    Create projetinfo1page_principal.js

---
+++ projetinfo1page_principal.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let lastScrollY = window.scrollY;  // Stocke la position de défilement précédente
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            // Vérifie si l'élément entre dans le viewport et si le défilement est vers le bas
+            if (entry.isIntersecting && window.scrollY > lastScrollY) {
+                entry.target.classList.add('fade-in');
+            } else {
+                entry.target.classList.remove('fade-in');
+            }
+        });
+        // Mise à jour de la dernière position de défilement après le traitement des entrées
+        lastScrollY = window.scrollY;
+    }, { threshold: 0.1 });
+
+    // Observe tous les éléments sélectionnés
+    document.querySelectorAll('.text, .bubble').forEach((section) => {
+        observer.observe(section);
+    });
+
+    // Mettre à jour lastScrollY chaque fois que l'utilisateur scroll
+    window.addEventListener('scroll', () => {
+        lastScrollY = window.scrollY;
+    });
+});
