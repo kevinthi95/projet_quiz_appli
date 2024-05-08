2c222dad4fb8d3d95947f7a08b02cecf5a8f3f5d
kevinthi95 <133572994+kevinthi95@users.noreply.github.com>
2024-05-09 00:12:49 +0200

    Update projetinfo1page_principal.php

--- projetinfo1page_principal.php
+++ projetinfo1page_principal.php
@@ -7,20 +7,36 @@
     <link rel="stylesheet" href="projetinfo1page_principal.css">
 </head>
 <body class="fade-in">
-    <header>
-        <h1>Présentation de la Formule 1</h1>
-        <nav>
-            <ul>
-                <li><a href="http://localhost:8888/page_avant_quiz.html">Quiz</a></li>
-                <li><a href="http://localhost:8888/index.php">Formulaire</a></li>
-                <li><a href="http://localhost:8888/classement.html">Classement</a></li>
-                <li><a href="http://localhost:8888/profil.php">Profil</a></li>
-                <li><a href="http://localhost:8888/deconnexion.php";>Se déconnecter</a></li>
+<?php
+// Démarrer la session
+session_start();
 
-            </ul>
-        </nav>
-    </header>
-    
+// Vérifier si l'ID de l'utilisateur est présent dans la session
+if(isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    var_dump($user_id);//test
+    // Utiliser $user_id pour personnaliser le contenu de la page ou pour toute autre fonctionnalité
+} else {
+    // Rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté
+    header("Location: index.php");
+    exit;
+}
+?>
+   <header>
+    <h1>Présentation de la Formule 1</h1>
+    <nav>
+        <ul>
+       
+
+            <li><a href="http://localhost:8888/page_avant_quiz.php?id=<?php echo $user_id; ?>" >Quiz</a></li>
+            <li><a href= "http://localhost:8888/index.php?id=<?php echo $user_id; ?>" >Formulaire</a></li>
+            <li><a href= "http://localhost:8888/classement.php?id=<?php echo $user_id; ?>" >Classement</a></li>
+            <li><a href= "http://localhost:8888/profile.php?id=<?php echo $user_id; ?>" >Profil</a></li>
+            <li><a href= "http://localhost:8888/deconnexion.php?id=$<?php echo $user_id; ?>">Se déconnecter</a></li>
+        </ul>
+    </nav>
+</header>
+
     <!-- Contenu principal -->
     <div class="text">
         <div class="information-section">
