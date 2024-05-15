0b6e1fc9c79576b895fad6127ebba422ab17d4a5
kevinthi95 <133572994+kevinthi95@users.noreply.github.com>
2024-05-15 20:09:40 +0200

    Create carte_abonne.js

---
+++ carte_abonne.js
@@ -0,0 +1,38 @@
+function ajouterCarre() {
+    var noteContainer = document.getElementById('noteContainer');
+    var nouvelleNote = document.createElement('div');
+    nouvelleNote.setAttribute('class', 'note');
+    nouvelleNote.setAttribute('contenteditable', 'true');
+    nouvelleNote.innerText = 'Cliquez pour écrire...';
+
+    nouvelleNote.addEventListener('focus', function() {
+        if (nouvelleNote.innerText === 'Cliquez pour écrire...') {
+            nouvelleNote.innerText = '';
+            nouvelleNote.style.color = 'black';
+        }
+    });
+
+    nouvelleNote.addEventListener('blur', function() {
+        if (nouvelleNote.innerText.trim() === '') {
+            nouvelleNote.innerText = 'Cliquez pour écrire...';
+            nouvelleNote.style.color = '#CCC';
+        }
+    });
+
+    // Gestionnaire pour agrandir/réduire le carré
+    nouvelleNote.addEventListener('click', function() {
+        // Vérifie si le carré est déjà agrandi
+        if (nouvelleNote.classList.contains('agrandi')) {
+            nouvelleNote.classList.remove('agrandi');
+        } else {
+            // Retire la classe 'agrandi' des autres notes avant d'agrandir la note courante
+            let toutesLesNotes = document.querySelectorAll('.note');
+            toutesLesNotes.forEach(note => {
+                note.classList.remove('agrandi');
+            });
+            nouvelleNote.classList.add('agrandi');
+        }
+    });
+
+    noteContainer.appendChild(nouvelleNote);
+}
