487b9d1b506426d29af121e4dd6f72c3cd69b093
CYShop <133589385+CYShop@users.noreply.github.com>
2024-05-17 14:17:39 +0200

    Update projetinfo1.php

--- projetinfo1.php
+++ projetinfo1.php
@@ -1,4 +1,17 @@
+<?php
+// Démarrer la session
+session_start();
 
+// Vérifier si l'ID de l'utilisateur est présent dans la session
+if(isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    // Utiliser $user_id pour personnaliser le contenu de la page ou pour toute autre fonctionnalité
+} else {
+    // Rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté
+    header("Location: http://localhost:8888/index.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -15,19 +28,18 @@
         <div id="difficulty-container">
             <div id="difficulty"></div>
         </div>
-        <div id="result-container"></div>
     </div>
     <div id="result-container"></div>
     <div id="legend-container">
         <p>Légende :</p>
         <div class="legend-item">
-            <span class="difficulty-circle difficulty-facile"></span> Facile (1 point)
+            <span class="difficulty-circle difficulty-facile"></span> Facile
         </div>
         <div class="legend-item">
-            <span class="difficulty-circle difficulty-intermédiaire"></span> Intermédiaire (2 points)
+            <span class="difficulty-circle difficulty-intermédiaire"></span> Intermédiaire
         </div>
         <div class="legend-item">
-            <span class="difficulty-circle difficulty-difficile"></span> Difficile (3 points)
+            <span class="difficulty-circle difficulty-difficile"></span> Difficile
         </div>
     </div>
     <div id="question-count">
@@ -36,8 +48,9 @@
     <div id="timer-container">
         Temps écoulé : <span id="time">00:00</span>
     </div>
-    
+
     <script src="projetinfo1.js"></script>
-    <a href="http://localhost:8888/projetinfo1page_principal.php?id=<?php echo $user_id; ?>"  class="center-link">Retour à la page principale</a>
+
+    <a href="http://localhost:8888/projetinfo1page_principal.php?id=<?php echo $user_id; ?>" class="center-link">Retour à la page principale</a>
 </body>
 </html>
