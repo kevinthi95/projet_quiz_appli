582ed94b63c86d4b9264faacc6fbcf467f8712d6
kevinthi95 <133572994+kevinthi95@users.noreply.github.com>
2024-05-17 14:08:13 +0200

    Update page_avant_quiz.php

--- page_avant_quiz.php
+++ page_avant_quiz.php
@@ -1,26 +1,65 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Présentation du Quiz</title>
-    <link rel="stylesheet" href="page_avant_quiz.css"> 
-</head>
-<body>
 <?php
-// Démarrer la session
 session_start();
 
 // Vérifier si l'ID de l'utilisateur est présent dans la session
 if(isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
-    // Utiliser $user_id pour personnaliser le contenu de la page ou pour toute autre fonctionnalité
 } else {
     // Rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté
-    header("Location: index.php");
+    header("Location: http://localhost:8888/index.php");
+    exit;
+}
+
+// Fonction pour récupérer l'email depuis le fichier utilisateurs.txt basé sur user_id
+function getEmailFromUserId($user_id) {
+    $filename = 'utilisateurs.txt';
+    if (file_exists($filename)) {
+        $lines = file($filename, FILE_IGNORE_NEW_LINES);
+        foreach ($lines as $line) {
+            $data = explode(';', $line);
+            if (trim($data[2]) == $user_id) {
+                return trim($data[0]);
+            }
+        }
+    }
+    return null;
+}
+
+$email = getEmailFromUserId($user_id);
+
+
+// Vérifier si l'utilisateur a le droit d'accès au quiz
+function checkAccess($email) {
+    $filename = "donnees/" . $email . ".txt";
+    if (file_exists($filename)) {
+        $lines = file($filename, FILE_IGNORE_NEW_LINES);
+        if (isset($lines[6])) { // La 7ème ligne est indexée par 6
+            $parts = explode(':', $lines[6]);
+            if (count($parts) >= 2 && trim($parts[1]) == "non") {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+if (!$email || !checkAccess($email)) {
+    // Afficher le message d'erreur et attendre 5 secondes avant la redirection
+    echo '<p>Vous n\'êtes pas habilité à accéder au quiz !</p>';
+    echo '<p>Redirection automatique dans 5 secondes...</p>';
+    header("refresh:5;url=http://localhost:8888/projetinfo1page_principal.php?id=$user_id");
     exit;
 }
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accès au Quiz</title>
+    <link rel="stylesheet" href="page_avant_quiz.css">
+</head>
+<body>
     <header>
         <h1>Quiz Formule 1</h1>
     </header>
@@ -41,3 +80,4 @@ if(isset($_SESSION['user_id'])) {
     <a href="http://localhost:8888/projetinfo1page_principal.php?id=<?php echo $user_id; ?>" class="center-link">Retour à la page principale</a>
 </body>
 </html>
+
