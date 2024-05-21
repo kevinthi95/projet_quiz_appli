787e49d22dd1bc3c417ac49c92da7141a77debe9
kevinthi95 <133572994+kevinthi95@users.noreply.github.com>
2024-05-21 08:18:14 +0200

    Update projetinfo1page_principal.php

--- projetinfo1page_principal.php
+++ projetinfo1page_principal.php
@@ -28,15 +28,17 @@ if(isset($_SESSION['user_id'])) {
             <li><a href="http://localhost:8888/page_avant_quiz.php?id=<?php echo $user_id; ?>" >Quiz</a></li>
             <li><a href= "http://localhost:8888/classement.php?id=<?php echo $user_id; ?>" >Classement</a></li>
             <li><a href= "http://localhost:8888/profil.php?id=<?php echo $user_id; ?>" >Profil</a></li>
-            <li><a href= "http://localhost:8888/deconnexion.php?id=$<?php echo $user_id; ?>">Se déconnecter</a></li>
             <li><a href="http://localhost:8888/administrateur.php?id=<?php echo $user_id; ?>">Admin</a></li>
             <li><a href="http://localhost:8888/page_avant_abonnement.php?id=<?php echo $user_id; ?>">Abonnement</a></li>
             <li><a href="http://localhost:8888/messagerie.php?id=<?php echo $user_id; ?>">Messagerie</a></li>
         </ul>
     </nav>
 </header>
-
-    <!-- Contenu principal -->
+<a href="http://localhost:8888/deconnexion.php?id=<?php echo $user_id; ?>" class="logout-button">
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M13 3H11V13H13V3ZM12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2ZM12 20C7.6 20 4 16.4 4 12C4 7.6 7.6 4 12 4C16.4 4 20 7.6 20 12C20 16.4 16.4 20 12 20Z"/>
+    </svg>
+</a>
     <div class="text">
         <div class="information-section">
             <h2>Informations Générales sur la Formule 1</h2>
