e6d39209470957f78af5dea286c7851b7b65aad2
kevinthi95 <133572994+kevinthi95@users.noreply.github.com>
2024-05-21 08:09:42 +0200

    Update projetinfo1page_principal.php

--- projetinfo1page_principal.php
+++ projetinfo1page_principal.php
@@ -358,7 +358,19 @@ if(isset($_SESSION['user_id'])) {
             </table>
         </div>
     </div>
-
+<button class="button" id="backToTopButton">
+        <svg class="svgIcon" viewBox="0 0 384 512">
+            <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"></path>
+        </svg>
+    </button>
+    <script>
+document.getElementById('backToTopButton').addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'  // Ce paramètre permet un défilement doux
+  });
+});
     <div class="container-flex ">
         <div class="bubble-flex">
             <div class="section-flex">
