5ed30c1648a2dea1915cfdca886fc19b78e6d18a
kevinthi95 <133572994+kevinthi95@users.noreply.github.com>
2024-05-22 17:41:47 +0200

    Update classement.php

--- classement.php
+++ classement.php
@@ -48,6 +48,7 @@ $topResults = array_slice($results, 0, 20);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="classement.css">
+        <link rel="stylesheet" href="style.css">
     <title>Classement</title>
 </head>
 <body>
