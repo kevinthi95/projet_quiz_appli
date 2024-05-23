1061bd1aebffe2bf2a1fe181df6cf590a378eebd
kevinthi95 <133572994+kevinthi95@users.noreply.github.com>
2024-05-23 17:21:00 +0200

    Update admin_content.php

--- admin_content.php
+++ admin_content.php
@@ -1,5 +1,5 @@
 <?php
-session_start(); // Démarre la session
+session_start(); 
 
 if(isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
@@ -8,27 +8,26 @@ if(isset($_SESSION['user_id'])) {
     exit;
 }
 
-// Le code ci-dessous est accessible uniquement aux administrateurs
+// on peut acces uniquement en admin
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Traitement pour bloquer ou débloquer l'accès au quiz
+        // bloquer /dénloquer
         if (isset($_POST['email-bloquer'])) {
-            // Traitement pour bloquer l'accès au quiz
+            // ici c'est bloquer
             $email = $_POST['email-bloquer'];
             $filename = 'donnees/' . $email . '.txt';
 
-            // Vérifie si le fichier de l'utilisateur existe
+            // existe?
             if (file_exists($filename)) {
-                // Lit le contenu du fichier
+            
                 $userData = file_get_contents($filename);
 
-                // Vérifie si l'accès est déjà bloqué ou non
                 if (strpos($userData, 'Accès:non') !== false) {
                     echo '<div id="message">Utilisateur déjà bloqué.</div>';
                 } else {
-                    // Met à jour l'attribut "Accès" dans le fichier de l'utilisateur
+                    //si accès existe alors accès non si on clique
                     $userData = preg_replace('/Accès:(.*)/', 'Accès:non', $userData);
 
-                    // Écrit le contenu mis à jour dans le fichier
+                    // met a jour
                     file_put_contents($filename, $userData);
 
                     echo '<div id="message">Accès au quiz bloqué pour l\'utilisateur avec l\'email: ' . $email . '</div>';
@@ -37,23 +36,20 @@ if(isset($_SESSION['user_id'])) {
                 echo '<div id="message">Utilisateur non trouvé.</div>';
             }
         } elseif (isset($_POST['email-debloquer'])) {
-            // Traitement pour débloquer l'accès au quiz
+            // debloquer 
             $email = $_POST['email-debloquer'];
             $filename = 'donnees/' . $email . '.txt';
 
-            // Vérifie si le fichier de l'utilisateur existe
+            // existe ?
             if (file_exists($filename)) {
-                // Lit le contenu du fichier
                 $userData = file_get_contents($filename);
 
-                // Vérifie si l'accès est déjà débloqué ou non
                 if (strpos($userData, 'Accès:oui') !== false) {
                     echo '<div id="message1">Utilisateur déjà débloqué.</div>';
                 } else {
-                    // Met à jour l'attribut "Accès" dans le fichier de l'utilisateur
+                   // acces  = oui 
                     $userData = preg_replace('/Accès:(.*)/', 'Accès:oui', $userData);
 
-                    // Écrit le contenu mis à jour dans le fichier
                     file_put_contents($filename, $userData);
 
                     echo '<div id="message1">Accès au quiz débloqué pour l\'utilisateur avec l\'email: ' . $email . '</div>';
@@ -76,14 +72,12 @@ if(isset($_SESSION['user_id'])) {
 <body>
 <div class="container">
     <h1>Page administrateur</h1>
-    <!-- Contenu accessible uniquement aux administrateurs -->
     <div>
         <h2>Bloquer l'accès au quiz</h2>
         <form method="post">
             <label for="email-bloquer">Choisir un utilisateur à bloquer:</label>
             <select name="email-bloquer" id="email-bloquer" required>
             <?php
-            // Lit le fichier utilisateurs.txt pour obtenir la liste des emails
             $filename = 'utilisateurs.txt';
             if (file_exists($filename)) {
                 $users = file($filename);
@@ -103,7 +97,6 @@ if(isset($_SESSION['user_id'])) {
             <label for="email-debloquer">Choisir un utilisateur à débloquer:</label>
             <select name="email-debloquer" id="email-debloquer" required>
             <?php
-            // Lit le fichier utilisateurs.txt pour obtenir la liste des emails
             $filename = 'utilisateurs.txt';
             if (file_exists($filename)) {
                 $users = file($filename);
