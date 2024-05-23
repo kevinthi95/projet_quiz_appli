a4d1854a3b7a249c982c62c793bc3f0f35fd7927
kevinthi95 <133572994+kevinthi95@users.noreply.github.com>
2024-05-23 17:45:26 +0200

    Update projetinfo1.php

--- projetinfo1.php
+++ projetinfo1.php
@@ -1,13 +1,9 @@
 <?php
-// Démarrer la session
 session_start();
 
-// Vérifier si l'ID de l'utilisateur est présent dans la session
 if(isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
-    // Utiliser $user_id pour personnaliser le contenu de la page ou pour toute autre fonctionnalité
 } else {
-    // Rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté
     header("Location: http://localhost:8888/index.php");
     exit;
 }
