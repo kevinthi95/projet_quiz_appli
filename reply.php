999d4fbad25a1ba8c95bc2247be1097b222fef9d
kevinthi95 <133572994+kevinthi95@users.noreply.github.com>
2024-05-23 17:53:08 +0200

    Update reply.php

--- reply.php
+++ reply.php
@@ -1,30 +1,25 @@
 <?php
-session_start(); // Démarre la session
+session_start(); 
 
-// Vérifie si l'ID de l'utilisateur est présent dans la session
 if(isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
-    // Utiliser $user_id pour personnaliser le contenu de la page ou pour toute autre fonctionnalité
 } else {
-    // Rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté
     header("Location: index.php");
     exit;
 }
 
-// Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email'], $_POST['reply'])) {
-    // Récupérer les données soumises par le formulaire
     $email = $_POST['email'];
     $replyMessage = $_POST['reply'];
 
-    // Assurez-vous que l'e-mail de l'utilisateur et la réponse ne sont pas vides
+    // l'e-mail et réponse non vides ?
     if (!empty($email) && !empty($replyMessage)) {
-        // Ajoutez la logique pour enregistrer la réponse dans le fichier de messages de l'utilisateur
+        // logique pour enregistrer la réponse dans le fichier de messages 
         $filename = 'message/' . $email . '.txt';
-        $reply = "Admin: " . $replyMessage; // Format du message de réponse (ici "Admin:" est ajouté pour indiquer que c'est une réponse de l'administrateur)
-        // Ajoutez la réponse au fichier de messages
+        $reply = "Admin: " . $replyMessage; 
+        // Ajoutez la réponse 
         file_put_contents($filename, $reply . PHP_EOL, FILE_APPEND);
-        // Rediriger vers la page d'administration après avoir répondu
+        // Rediriger vers la page d'administration 
         header("Location: adminmess.php");
         exit;
     } else {
@@ -33,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email'], $_POST['reply
     }
 } else {
     // Si le formulaire n'a pas été soumis correctement, rediriger vers la page principale ou afficher un message d'erreur
-    header("Location: index.php"); // Redirection vers la page principale
+    header("Location: index.php"); 
     exit;
 }
 ?>
